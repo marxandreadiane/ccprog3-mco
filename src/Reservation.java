c6a3b18e823cbf152f454a8dfbb7a76743a13351
marxandreadiane <135679920+marxandreadiane@users.noreply.github.com>
2024-06-23 03:10:16 +0800

    with javadoc Reservation.java

--- src/Reservation.java
+++ src/Reservation.java
@@ -1,3 +1,6 @@
+/**
+ * Represents a hotel reservation.
+ */
 public class Reservation
 {
     private String guestName;
@@ -6,6 +9,12 @@ public class Reservation
     private Room room;
     private double total;
 
+    /**
+     * Constructs a Reservation with the provided guest name, check-in date, and check-out date.
+     * @param guestName the name of the guest
+     * @param checkInDate the check-in date
+     * @param checkOutDate the check-out date
+     */
     public Reservation(String guestName, int checkInDate, int checkOutDate)
     {
         this.guestName = guestName;
@@ -13,36 +22,63 @@ public class Reservation
         this.checkOutDate = checkOutDate;
     }
 
+    /**
+     * Gets the name of the guest.
+     * @return the guest's name
+     */
     public String getGuestName()
     {
         return this.guestName;
     }
 
+    /**
+     * Gets the check-in date.
+     * @return the check-in date
+     */
     public int getCheckInDate()
     {
         return this.checkInDate;
     }
 
+    /**
+     * Gets the check-out date.
+     * @return the check-out date
+     */
     public int getCheckOutDate()
     {
         return this.checkOutDate;
     }
 
+    /**
+     * Gets the room associated with this reservation.
+     * @return the room
+     */
     public Room getRoom()
     {
         return this.room;
     }
 
+    /**
+     * Sets the room associated with this reservation.
+     * @param room the room to set this reservation with
+     */
     public void setRoom(Room room)
     {
         this.room = room;
     }
 
+    /**
+     * Gets the total price for this reservation.
+     * @return the total price
+     */
     public double getTotal()
     {
         return (this.checkOutDate - this.checkInDate) * this.room.getPrice();
     }
 
+    /**
+     * Prints information about this reservation.
+     */
     public void printReservationInfo()
     {
         System.out.print("Name of guest: " + getGuestName());
