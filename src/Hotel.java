69b600e93f1e05eb62321d31b3ec182db63a5d4d
marxandreadiane <135679920+marxandreadiane@users.noreply.github.com>
2024-06-23 03:11:54 +0800

    with javadoc Hotel.java

--- src/Hotel.java
+++ src/Hotel.java
@@ -1,7 +1,9 @@
-
 import java.util.ArrayList;
 import java.util.Scanner;
 
+/**
+ * Represents a Hotel with a name, a list of rooms, and a list of reservations.
+ */
 public class Hotel
 {
     private String name;
@@ -20,11 +22,126 @@ public class Hotel
 
 
     // GETTERS and SETTERS
+    /**
+     * Gets the name of this hotel.
+     * @return the name of this hotel
+     */
     public String getName()
     {
         return this.name;
     }
 
+    /**
+     * Sets the name of this hotel.
+     * @param name the new name of this hotel
+     */
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+
+    /**
+     * Gets the total estimated earnings from all reservations.
+     * @return the total earnings from reservations
+     */
+    private double getTotalReservation()
+    {
+        double total = 0;
+
+        for (Reservation reservation : reservationList)
+        {
+            total += reservation.getTotal();
+        }
+
+        return total;
+    }
+
+    /**
+     * Gets the number of available rooms for a given date range.
+     * @param checkInDate the check-in date
+     * @param checkOutDate the check-out date
+     * @return the number of available rooms
+     */
+    public int getAvailableRooms(int checkInDate, int checkOutDate)
+    {
+        int nRooms = 0;
+
+        for (Room room : roomList)
+        {
+            if (room.getAvailability(checkInDate, checkOutDate) == true)
+            {
+                nRooms++;
+            }
+        }
+        return nRooms;
+    }
+    
+    /**
+     * Gets the number of available rooms for a specific date.
+     * @param date the date to check availability
+     * @return the number of available rooms
+     */
+    private int getAvailableRooms(int date)
+    {
+        int nRooms = 0;
+
+        for (Room room : roomList)
+        {
+            if (room.getAvailability(date) == true)
+            {
+                nRooms++;
+            }
+        }
+        return nRooms;
+    }
+
+    /**
+     * Gets the number of booked rooms for a specific date.
+     * @param date the date to check bookings
+     * @return the number of booked rooms
+     */
+    private int getBookedRooms(int date)
+    {
+        int nRooms = 0;
+
+        for (Room room : roomList)
+        {
+            if (room.getAvailability(date) == false)
+            {
+                nRooms++;
+            }
+        }
+        return nRooms;
+    }
+
+    /**
+     * Sets the base price for all rooms in this hotel.
+     * @param price the base price to set
+     * @return true if the price is set successfully, false if the price is less than 100
+     */
+    public boolean setBasePrice(double price)
+    {
+        boolean success = true;
+
+        if (price >= 100)
+        {
+            for (Room room : roomList)
+            {
+                room.setPrice(price);
+            }
+        }
+        else
+        {
+            success = false;
+        }
+
+        return success;
+    }
+
+    /**
+     * Prints high-level information about this hotel, including name, total number of rooms,
+     * and estimated earnings for the month.
+     */
     public void getHighLevel()
     {
         String hotelInformation = "Hotel Name: " + getName() + 
@@ -34,6 +151,9 @@ public class Hotel
         System.out.println(hotelInformation);
     }
 
+    /**
+     * Provides a menu for low-level hotel information.
+     */
     public void getLowLevel()
     {
         //String hotelInformation;
@@ -105,85 +225,12 @@ public class Hotel
         } while (choice != 4);
     }
 
-    private double getTotalReservation()
-    {
-        double total = 0;
-
-        for (Reservation reservation : reservationList)
-        {
-            total += reservation.getTotal();
-        }
-
-        return total;
-    }
-    
-    public void setName(String name)
-    {
-        this.name = name;
-    }
-
-    public int getAvailableRooms(int checkInDate, int checkOutDate)
-    {
-        int nRooms = 0;
-
-        for (Room room : roomList)
-        {
-            if (room.getAvailability(checkInDate, checkOutDate) == true)
-            {
-                nRooms++;
-            }
-        }
-        return nRooms;
-    }
-    
-    private int getAvailableRooms(int date)
-    {
-        int nRooms = 0;
-
-        for (Room room : roomList)
-        {
-            if (room.getAvailability(date) == true)
-            {
-                nRooms++;
-            }
-        }
-        return nRooms;
-    }
-
-    private int getBookedRooms(int date)
-    {
-        int nRooms = 0;
-
-        for (Room room : roomList)
-        {
-            if (room.getAvailability(date) == false)
-            {
-                nRooms++;
-            }
-        }
-        return nRooms;
-    }
-
-    public boolean setBasePrice(double price)
-    {
-        boolean success = true;
-
-        if (price >= 100)
-        {
-            for (Room room : roomList)
-            {
-                room.setPrice(price);
-            }
-        }
-        else
-        {
-            success = false;
-        }
-
-        return success;
-    }
-
     // ADD or DELETE METHODS
+    /**
+     * Adds a room to this hotel.
+     * @param name the name of the room to add
+     * @return true if the room is added successfully, false if the room already exists
+     */
     public boolean addRoom(String name)
     {
         boolean success = false;
@@ -197,6 +244,11 @@ public class Hotel
         return success;
     }
 
+    /**
+     * Removes a room from this hotel.
+     * @param name the name of the room to remove
+     * @return true if the room is removed successfully, false if the room does not exist
+     */
     public boolean removeRoom(String name)
     {
         boolean success = false;
@@ -209,6 +261,13 @@ public class Hotel
         return success;
     }
 
+    /**
+     * Creates a reservation for a guest.
+     * @param guestName the name of the guest
+     * @param checkInDate the check-in date
+     * @param checkOutDate the check-out date
+     * @return true if the reservation is created successfully, false otherwise
+     */
     public boolean createReservation(String guestName, int checkInDate, int checkOutDate)
     {
         Reservation reservation;
@@ -220,8 +279,9 @@ public class Hotel
             if (roomList.get(i).getAvailability(checkInDate, checkOutDate))
             {
                 reservation = new Reservation(guestName, checkInDate, checkOutDate);
+                roomList.get(i).bookRoom(checkInDate, checkOutDate);
                 reservation.setRoom(roomList.get(i));
-                
+
                 reservationList.add(reservation);
                 success = true;
             }
@@ -231,28 +291,38 @@ public class Hotel
         return true;
     }
 
+    /**
+     * Removes a reservation for a guest.
+     * @param guestName the name of the guest
+     * @param checkInDate the check-in date
+     * @param checkOutDate the check-out date
+     * @return true if the reservation is removed successfully, false otherwise
+     */
     public boolean removeReservation(String guestName, int checkInDate, int checkOutDate)
     {
         boolean success = false;
-        int i = 0;
+        int i;
 
-        for (Reservation reservation : reservationList)
+        for (i = 0; i < reservationList.size(); i++)
         {
-            if (reservation.getGuestName().equals(guestName)
-                && reservation.getCheckInDate() == checkInDate
-                && reservation.getCheckOutDate() == checkOutDate
-                && !success)
+            if (reservationList.get(i).getGuestName().equals(guestName)
+                && reservationList.get(i).getCheckInDate() == checkInDate
+                && reservationList.get(i).getCheckOutDate() == checkOutDate)
             {
                 reservationList.remove(i);
                 success = true;
             }
-            i++;
         }
         
         return success;
     }
 
     // SEARCH METHODS
+    /**
+     * Finds a room by its name.
+     * @param name the name of the room to find
+     * @return the room with the specified name, or null if no such room exists
+     */
     public Room findRoomByName(String name)
     {
         for (Room room : roomList)
@@ -266,16 +336,20 @@ public class Hotel
         return null;
     }
 
+    /**
+     * Filters reservations by guest name.
+     * @param name the name of the guest
+     * @return a list of reservations for the specified guest
+     */
     public ArrayList<Reservation> filterReservationByName(String name)
     {
-        int i;
         ArrayList<Reservation> filteredList = new ArrayList<Reservation>();
-        
-        for (i = 0; i < reservationList.size(); i++)
+
+        for (Reservation reservation : this.reservationList)
         {
-            if (reservationList.get(i).getGuestName().equals(name))
+            if (reservation.getGuestName().equals(name))
             {
-                filteredList.add(reservationList.get(i));
+                filteredList.add(reservation);
             }
         }
 
